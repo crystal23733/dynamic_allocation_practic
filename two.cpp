551316cffdedd45d029e8e766c1f268d47b2c05a
Hoyeong Yoo <crystal23733@gmail.com>
2025-07-21 00:39:07 +0900

    깊은 복사 연습

--- two.cpp
+++ two.cpp
@@ -7,12 +7,26 @@ int main() {
    * 메모리 주소를 복사하고 두 포인터는 같은 주소를 가리킴
    * "참조만 복사되었다"
    */
+  // int *a = new int(3);
+  // int *b = a;
+  // *a = 5;
+  // cout << "a: " << *a << endl;
+  // cout << "b: " << *b << endl;
+
+  // delete a;
+  // 이후 b를 사용하게되면 b는 삭제된 a를 가리키고있기 때문에 dangling pointer가 됨
+
+  /**
+   * 깊은 복사
+   * 새로운 공간을 가리키고 해당 포인터의 값만 복사
+   */
   int *a = new int(3);
-  int *b = a;
+  int *b = new int(*a);
   *a = 5;
+
   cout << "a: " << *a << endl;
   cout << "b: " << *b << endl;
 
   delete a;
-  // 이후 b를 사용하게되면 b는 삭제된 a를 가리키고있기 때문에 dangling pointer가 됨
+  delete b;
 }
