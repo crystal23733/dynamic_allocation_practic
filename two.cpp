646ca8c51ac6712b7f7759847466722aa7c08f17
Hoyeong Yoo <crystal23733@gmail.com>
2025-07-21 00:10:44 +0900

    얕은 복사 연습

--- two.cpp
+++ two.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
 using namespace std;
 
-// 얕은 복사
+int main() {
+  /**
+   * 얕은 복사
+   * 메모리 주소를 복사하고 두 포인터는 같은 주소를 가리킴
+   * "참조만 복사되었다"
+   */
+  int *a = new int(3);
+  int *b = a;
+  cout << "a: " << *a << endl;
+  cout << "b: " << *b << endl;
+
+  delete a;
+  // 이후 b를 사용하게되면 b는 삭제된 a를 가리키고있기 때문에 dangling pointer가 됨
+}
